67f65bed88a8c70703e1ae7654eaf59017f14872
kaanakgundogdu <kaanakgundogdu@gmail.com>
2022-05-16 21:09:48 +0300

    Random number generetor and shuffle func added.

--- Go-Language/2-Cards/deck.go
+++ Go-Language/2-Cards/deck.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 //Create new  type of deck
@@ -58,3 +60,13 @@ func newDeckFromFile(fileName string) deck {
 	return deck(s)
 }
 
+func (d deck)shuffle(){
+
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d{
+		newPosition := r.Intn(len(d)-1)
+		d[i], d[newPosition] = d[newPosition],d[i]//very fancy
+	}
+}
--- Go-Language/2-Cards/main.go
+++ Go-Language/2-Cards/main.go
@@ -1,15 +1,19 @@
 package main
 
 func main() {
-	// cards := newDeck()
+	cards := newDeck()
 	// hand, remainingCards := deal(cards, 5)
 
 	// hand.printDeck()
 	// remainingCards.printDeck()
 	// fmt.Println(cards.toString())
 	// cards.saveToFile("my_cards")
-	cards := newDeckFromFile("my_cards")
+	// cards := newDeckFromFile("my_cards")
+
+	cards.shuffle()
+
 	cards.printDeck()
+
 }
 
 // func newCard() string {
