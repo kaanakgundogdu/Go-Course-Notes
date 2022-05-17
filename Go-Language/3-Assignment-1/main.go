b12b704b0bc9c1de87f15fb6fc50b80fa09f2f59
kaanakgundogdu <kaanakgundogdu@gmail.com>
2022-05-17 18:08:59 +0300

    Assignment 1 added.

---
+++ Go-Language/3-Assignment-1/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	numbers := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10}
+
+	for index := range numbers {
+		if index%2 == 0 {
+			fmt.Printf("%v is even\n" , index)
+		} else {
+			fmt.Printf("%v is odd\n" , index)
+		}
+	}
+
+}
